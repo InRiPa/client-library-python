37780b9e7f12a129fe27226c978db20d89236062
Ingolf Gehrhardt <ingolf.gehrhardt@htw-dresden.de>
2021-05-05 19:01:58 +0200

    preconfigured provider and consumer

--- examples/consumer_app.py
+++ examples/consumer_app.py
@@ -2,16 +2,32 @@
 HttpConsumer example app
 """
 import arrowhead_client.api as ar
+# from arrowhead_client.system import ArrowheadSystem
+from arrowhead_client.system import ArrowheadSystem
+
+
+my_config = {
+        "service_registry": ArrowheadSystem(system_name='service_registry', address='141.56.139.44', port=8443, authentication_info=''),
+        "orchestrator": ArrowheadSystem(system_name='orchestrator', address='141.56.139.44', port=8441, authentication_info=''),
+        "authorization": ArrowheadSystem(system_name='authorization', address='141.56.139.44', port=8455, authentication_info=''),
+        "eventhandler": ArrowheadSystem(system_name='eventhandler', address='141.56.139.44', port=8455, authentication_info=''),
+        "gatekeeper": ArrowheadSystem(system_name='gatekeeper', address='141.56.139.44', port=8449, authentication_info=''),
+        "gateway": ArrowheadSystem(system_name='gatekeeper', address='141.56.139.44', port=8453, authentication_info='')
+    }
 
 consumer_app = ar.ArrowheadHttpClient(
-        system_name='example-consumer',
+        system_name='timeconsumer',
         address='127.0.0.1',
         port=7656,
-        keyfile='certificates/example-consumer.key',
-        certfile='certificates/example-consumer.crt',
+        config=my_config,
+        keyfile='certificates/timeconsumer.key',
+        certfile='certificates/timeconsumer.crt',
 )
 
 consumer_app.add_consumed_service('hello-arrowhead', 'GET')
+
+
+
 
 if __name__ == '__main__':
     response = consumer_app.consume_service('hello-arrowhead')
--- examples/provider_app.py
+++ examples/provider_app.py
@@ -2,23 +2,38 @@
 HttpProvider example app
 """
 import arrowhead_client.api as ar
+# from arrowhead_client.system import ArrowheadSystem
+from arrowhead_client.system import ArrowheadSystem
+import time
+
+my_config = {
+        "service_registry": ArrowheadSystem(system_name='service_registry', address='141.56.139.44', port=8443, authentication_info=''),
+        "orchestrator": ArrowheadSystem(system_name='orchestrator', address='141.56.139.44', port=8441, authentication_info=''),
+        "authorization": ArrowheadSystem(system_name='authorization', address='141.56.139.44', port=8455, authentication_info=''),
+        "eventhandler": ArrowheadSystem(system_name='eventhandler', address='141.56.139.44', port=8455, authentication_info=''),
+        "gatekeeper": ArrowheadSystem(system_name='gatekeeper', address='141.56.139.44', port=8449, authentication_info=''),
+        "gateway": ArrowheadSystem(system_name='gatekeeper', address='141.56.139.44', port=8453, authentication_info='')
+    }
 
 provider_app = ar.ArrowheadHttpClient(
-        system_name='example-provider',
+        system_name='provider1',
         address='127.0.0.1',
         port=7655,
-        keyfile='certificates/example-provider.key',
-        certfile='certificates/example-provider.crt',
+        config=my_config,
+        keyfile='certificates/provider1.key',
+        certfile='certificates/provider1.crt',
 )
 
 
 @provider_app.provided_service(
         'hello-arrowhead',
         'hello',
-        'HTTP-INSECURE-JSON',
+        'HTTP-SECURE-JSON',
         'GET', )
 def hello_arrowhead(request):
     return {"msg": "Hello, Arrowhead!"}
+
+
 
 if __name__ == '__main__':
     provider_app.run_forever()
